23b00f074c234d3efa0a23d7b3ae4f19c70b7edc
Timothy Rotich <tim.chris42@gmail.com>
2022-09-08 14:33:07 -0700

    Tutorials and Practice code on C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-/*
- *Printing texts using functions in C
+/**
  *
- * */
+ *  * main  is the start of Entry point
+ *
+ *   *
+ *
+ *    * Return: Shoul return zero on success
+ *
+ *     */
 
 int main(void)
 
