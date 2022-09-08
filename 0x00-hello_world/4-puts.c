0bd1843b3f93bae355e213970741f719d1142df8
Timothy Rotich <tim.chris42@gmail.com>
2022-09-08 14:28:52 -0700

    Tutorials and Practice code on C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ *Printing texts using functions in C
+ *
+ * */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
