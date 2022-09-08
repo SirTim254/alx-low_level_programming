a9bd45ad546804b730f60e79d0f4424fd202674d
Timothy Rotich <tim.chris42@gmail.com>
2022-09-08 14:56:00 -0700

    Tutorials and Practice code on C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-
-
-
 /**
  *main - Entry point
  *puts -Prints "Progamming is like building a multilingual puzzle"
@@ -11,9 +8,7 @@
 int main(void)
 
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+		return (0);
 
 }
