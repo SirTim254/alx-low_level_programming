325fd44fd7c071fa102d09cd11d46865ae9524cd
Timothy Rotich <tim.chris42@gmail.com>
2022-09-09 16:15:49 -0700

    Shell redirection

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <time.h>
+
+
+
 /**
-*main-Programme entry point
-*Return:Return zero on success 
-*/
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+
+
 int main(void)
+
 {
-int i;
-for(i = 0; i < 10; i++)
+
+		int i;
+
+
+
+for (i = 0; i < 10; i++)
 printf("%d", i);
 printf("\n");
 return (0);
