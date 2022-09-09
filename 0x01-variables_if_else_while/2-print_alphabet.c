352bfd9056666299794ef80ff19db1da4fecb074
Timothy Rotich <tim.chris42@gmail.com>
2022-09-09 15:00:41 -0700

    Shell redirection

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Start of the programme
+ *Return:Return zero on success
+ */
+
+
+
+int main(void)
+
+{
+
+		char low;
+
+
+
+			for (low = 'a'; low <= 'z'; low++)
+
+						putchar(low);
+
+					putchar('\n');
+
+
+
+						return (0);
+
+}
