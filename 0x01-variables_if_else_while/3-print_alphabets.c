37873ebf6058f974733c81037c431412ac935ddb
Timothy Rotich <tim.chris42@gmail.com>
2022-09-09 15:24:01 -0700

    Shell redirection

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,12 +6,25 @@
  *Return:Return Zero on success
  */
 int main(void)
+
 {
-	char low;
-		for (low = 'a'; low <= 'z'; low++)
-			putchar(low);
-			for (low = 'A'; low <= 'Z'; low++)
-			putchar(low);
-			putchar('\n');
-			return (0);
+
+		char low;
+
+
+
+			for (low = 'a'; low <= 'z'; low++)
+
+						putchar(low);
+
+				for (low = 'A'; low <= 'Z'; low++)
+
+							putchar(low);
+
+					putchar('\n');
+
+
+
+						return (0);
+
 }
