6e88b7be9c9e513f690e4a0978237797714391b5
Timothy Rotich <tim.chris42@gmail.com>
2022-09-09 13:57:46 -0700

    Shell redirection

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,18 @@
+#include <stdio.h>
+
 #include <stdlib.h>
 
 #include <time.h>
 
-/* more headers goes there */
-
-
-
-/* betty style doc for function main goes there */
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
 
@@ -20,21 +26,22 @@ int main(void)
 
 				n = rand() - RAND_MAX / 2;
 
-if (n > 0)
-
-			printf("%d is positive\n", n);
-
-	else if (n == 0)
-
-				printf("%d is zero\n", n);
-
-		else
-
-					printf("%d is negative\n", n);
 
 
+					if (n > 0)
 
-			return (0);
-				return (0);
+								printf("%d is positive\n", n);
+
+						else if (n == 0)
+
+									printf("%d is zero\n", n);
+
+							else
+
+										printf("%d is negative\n", n);
+
+
+
+								return (0);
 
 }
