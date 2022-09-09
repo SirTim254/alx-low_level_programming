cf56feaff6eefedce07c2b38ea62e279d34dcf0b
Timothy Rotich <tim.chris42@gmail.com>
2022-09-09 14:03:48 -0700

    Shell redirection

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - Entry point
+ *Return: on success return to main always zero on success
+ */
 
 int main(void)
 
