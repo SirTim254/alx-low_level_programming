478d256ffe986947724f8c232a3b8a11105db8bd
Timothy Rotich <tim.chris42@gmail.com>
2022-09-14 07:19:06 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- *Main: Start of the programme
+ *Main- Start of the programme
+ *main: strat of the main programme that call the function in the loop
  *_putchar - writes the character c to standard output
  *main- The character to print
  *Return: return zeroo on sucess
