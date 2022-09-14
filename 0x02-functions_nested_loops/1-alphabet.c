c5413c05cc7f06c8e43091a1a9aeae551aff6c1f
Timothy Rotich <tim.chris42@gmail.com>
2022-09-14 12:39:25 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,19 @@
  * Return: Retuen 0 on successful execution
  *
  */
+
 void print_alphabet(void)
+
 {
+
 	char alpha;
+
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 {
+	
 	_putchar(alpha);
 }
+
 	_putchar('\n');
+
 }
