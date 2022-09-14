e4bd406884a0cf06a55ade0f19d1ec7d4a03cd5d
Timothy Rotich <tim.chris42@gmail.com>
2022-09-14 13:53:56 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,34 +15,29 @@ int print_sign(int n)
 
 		if (n > 0)
 
-				{
+		{
 
-							_putchar('+');
+			_putchar('+');
 
-									return (1);
+			return (1);
 
-										}
+		}
 
-			else if (n == 0)
+		else if (n == 0)
 
-					{
+		{
 
-								_putchar('0');
+			_putchar('0');
 
-										return (0);
+			return (0);
+		}
+		else if (n < 0)
 
-											}
+		{
+			_putchar('-');
 
-				else if (n < 0)
+			return (-1);
+		}
 
-						{
-
-									_putchar('-');
-
-											return (-1);
-
-												}
-
-					return (0);
-
+			return (0);
 }
