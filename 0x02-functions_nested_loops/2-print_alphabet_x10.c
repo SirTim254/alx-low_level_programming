0cd6d6247f21faf52f641cd6d5dd57129b0686e3
Timothy Rotich <tim.chris42@gmail.com>
2022-09-14 13:14:32 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,15 @@ void print_alphabet_x10(void)
 
 		int alpha;
 
-		int alpha2;
+			int alpha2;
 
-for (alpha = 0; alpha <= 9; alpha++)
 
-for (alpha2 = 'a'; alpha2 <= 'z'; alpha2++)
+
+				for (alpha = 0; alpha <= 9; alpha++)
+
+						{
+
+									for (alpha2 = 'a'; alpha2 <= 'z'; alpha2++)
 
 												{
 
