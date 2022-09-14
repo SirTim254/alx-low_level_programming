6faa7eaf393190cdab5e66edcfc4ecae138aa24f
Timothy Rotich <tim.chris42@gmail.com>
2022-09-14 07:15:03 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,8 +3,7 @@
 /**
  *Main - Start of the programme
  *_putchar - writes the character c to standard output
- *@c: The character to print
- *@sh: The character to print
+ *main: The character to print
  *Return: return zeroo on sucess
  */
 
