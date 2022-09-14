2536ca3ad87a09c41fca4a54862de458c9a022b2
Timothy Rotich <tim.chris42@gmail.com>
2022-09-14 13:46:02 -0700

    Functions and Nested Loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ *print_sign - prints the sign of a number
+ *
+ *@n: number being tested
+ *
+ *Return: return zero on success.
+ */
+
+
+
+int print_sign(int n)
+
+{
+
+		if (n > 0)
+
+				{
+
+							_putchar('+');
+
+									return (1);
+
+										}
+
+			else if (n == 0)
+
+					{
+
+								_putchar('0');
+
+										return (0);
+
+											}
+
+				else if (n < 0)
+
+						{
+
+									_putchar('-');
+
+											return (-1);
+
+												}
+
+					return (0);
+
+}
