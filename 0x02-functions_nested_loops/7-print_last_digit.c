dc66c16c50e93c0a00546a3b24b2aad087e54b15
Timothy Rotich <tim.chris42@gmail.com>
2022-09-14 14:26:49 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,25 +6,13 @@
  *Return: Return zero on success.
  */
 int print_last_digit(int nld)
-
 {
-
-		int pld;
-
-
-
-			pld = (nld % 10);
-
-
-
-		if (pld < 0)
-				{
-				pld = (-1 * pld);
-				}
-
-
-				_putchar(pld + '0');
-
-		return (pld);
-
+	int pld;
+	pld = (nld % 10);
+	if (pld < 0)
+	{
+	pld = (-1 * pld);
+	}
+	_putchar(pld + '0');
+	return (pld);
 }
