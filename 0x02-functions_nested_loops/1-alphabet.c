8a4362e2ae191a3a9dd570151e008128c104ba16
Timothy Rotich <tim.chris42@gmail.com>
2022-09-14 12:53:35 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_alphabet - Prints the alphabet letters in lowercase 
- * Return: Retuen 0 on successful execution
- */
+* print_alphabet - Prints the alphabet letters in lowercase
+* Return: Retuen 0 on successful execution
+*/
 void print_alphabet(void)
 {
 	char alpha;
