9449599328985459b64f3f2b3b1daf7b8a2bbff4
Timothy Rotich <tim.chris42@gmail.com>
2022-09-14 19:45:35 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,8 +20,9 @@ void times_table(void)
 				{
 				_putchar(',');
 				_putchar(' ');																}
-																					if (y == 0)																{
-				_putchar('0');
+																					if (y == 0)
+																					{
+																						_putchar('0');
 																					}
 																					else if (product >= 10)																		{																	_putchar((product / 10) + '0');
  				_putchar((product % 10) + '0');
