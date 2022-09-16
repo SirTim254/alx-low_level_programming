e3510cea6cf197a8ab54e78ec9db7d9a0172b069
Timothy Rotich <tim.chris42@gmail.com>
2022-09-16 02:47:19 -0700

    More functions in C

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,15 +15,15 @@ int i = 0, j, n = size - 1;
 		{
 			for (j = 0; j < size; j++)
 			{
-																					if (j < n)
-			_putchar(' ');
-			else
-			_putchar('#');
+				if (j < n)
+					_putchar(' ');
+				else
+					_putchar('#');
 			}
 			n--;
 			_putchar('\n');
 		}
 	}
-		else
-			_putchar('\n');
+	else
+		_putchar('\n');
 }
